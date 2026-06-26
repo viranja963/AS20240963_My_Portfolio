7e37fb7713dfef01ce06e58a7b4c98489d0c5409
viranja963 <as20240963@sci.sjp.ac.lk>
2026-06-27 00:54:11 +0530

    add navbar scroll effect and mobile hamburger effect

--- script.js
+++ script.js
@@ -61,3 +61,23 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
+
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+
+
+
+hamburger.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
+
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function() {
+        navLinks.classList.remove('active');
+        hamburger.classList.remove('active');
+    });
+});
